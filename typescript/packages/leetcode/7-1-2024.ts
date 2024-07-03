8be885c7dcd02b87c1f55e7e7bf9a02eeee30801
benjamin <benschac@gmail.com>
2024-07-02 21:13:35 -0400

    add yesterdays leetcode

--- typescript/packages/leetcode/7-1-2024.ts
+++ typescript/packages/leetcode/7-1-2024.ts
@@ -40,5 +40,49 @@ function threeConsecutiveOdds(arr: number[]): boolean {
   return consecutiveCount >= 3;
 }
 
-console.log(threeConsecutiveOdds([2, 6, 4, 1]));
-console.log(threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]));
+// // console.log(threeConsecutiveOdds([2, 6, 4, 1]));
+// console.log(threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]));
+// https://leetcode.com/problems/word-break/
+
+/**
+ *
+ *Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+ * @param wordDict
+ * @returns
+ */
+
+function wordBreak(s: string, wordDict: string[]): boolean {
+  return false;
+}
+
+console.log(wordBreak("leetcode", ["leet", "code"]));
+console.log(wordBreak("applepenapple", ["apple", "pen"]));
